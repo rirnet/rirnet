74cd7b336db1af965101bb534b3796e274f51cb1
Felix <felixwiberg@gmail.com>
2018-09-03 16:36:25 +0200

    generate_room is now a module, added an example in tests

--- code/generate_room.py
+++ code/generate_room.py
@@ -2,19 +2,19 @@ import numpy as np
 import shapely.geometry as geo
 import matplotlib.pyplot as plt
 
-min_side = 3
-max_side = 10
+def generate_floor(min_side, max_side):
+    #min_side = 3
+    #max_side = 10
 
-maxx1, maxy1 = np.random.uniform(min_side, max_side, 2)
-minx2 = np.random.uniform(0, maxx1)
-miny2 = np.random.uniform(0, maxy1)
-maxx2 = np.random.uniform(minx2, max_side)
-maxy2 = np.random.uniform(miny2, max_side)
+    maxx1, maxy1 = np.random.uniform(min_side, max_side, 2)
+    minx2 = np.random.uniform(0, maxx1)
+    miny2 = np.random.uniform(0, maxy1)
+    maxx2 = np.random.uniform(minx2+min_side, max_side)
+    maxy2 = np.random.uniform(miny2+min_side, max_side)
 
-box1 = geo.box(0, 0, maxx1, maxy1)
-box2 = geo.box(minx2, miny2, maxx2, maxy2)
+    box1 = geo.box(0, 0, maxx1, maxy1)
+    box2 = geo.box(minx2, miny2, maxx2, maxy2)
 
-vertices = box1.union(box2).exterior.coords
+    vertices = box1.union(box2).exterior.coords
 
-plt.plot(vertices.xy[0], vertices.xy[1])
-plt.show()
+    return vertices.xy[0][:-1], vertices.xy[1][:-1]
