e733d8212f73207d2ed97316c36d9f383e2ec06f
Felix Viberg <felixwiberg@gmail.com>
2018-09-04 15:31:56 +0200

    renovated generate_room

--- code/generate_room.py
+++ code/generate_room.py
@@ -1,23 +1,52 @@
 import numpy as np
 import shapely.geometry as geo
-import matplotlib.pyplot as plt
 
 def generate_floor(min_side, max_side):
-    #min_side = 3
-    #max_side = 10
+    p_extrude = .5 #else subtract
+    box_x1 =box_y1 = 0
+    box_x2, box_y2 = np.random.uniform(min_side, max_side, 2)
+    box = geo.box(box_x1, box_y1, box_x2, box_y2)
 
-    maxx1, maxy1 = np.random.uniform(min_side, max_side, 2)
-    minx2 = np.random.uniform(0, maxx1)
-    miny2 = np.random.uniform(0, maxy1)
-    maxx2 = np.random.uniform(minx2+min_side, max_side)
-    maxy2 = np.random.uniform(miny2+min_side, max_side)
-
-    box1 = geo.box(0, 0, maxx1, maxy1)
-    box2 = geo.box(minx2, miny2, maxx2, maxy2)
-
-    if np.random.uniform() < .5:
-        vertices = box1.union(box2).exterior.coords
+    if np.random.uniform() < p_extrude:
+        x_coords, y_coords = extrude_box(box, box_x1, box_y1, box_x2, box_y2, min_side, max_side)
     else:
-        vertices = box1.difference(box2).exterior.coords
+        x_coords, y_coords = intrude_box(box, box_x1, box_y1, box_x2, box_y2, min_side, max_side)
+    return x_coords, y_coords
 
-    return vertices.xy[0][:-1], vertices.xy[1][:-1]
+
+def extrude_box(box1, box1_x1, box1_y1, box1_x2, box1_y2, min_side, max_side):
+
+    box2_x1 = np.random.uniform(box1_x1, box1_x2)
+    box2_y1 = np.random.uniform(box1_y1, box1_y2)
+
+    box2_x2 = np.random.uniform(box2_x1+min_side, max_side)
+    box2_y2 = np.random.uniform(box2_y1+min_side, max_side)
+
+    box2 = geo.box(box2_x1, box2_y1, box2_x2, box2_y2)
+
+    vertices = box1.union(box2).exterior.coords
+
+    x_coords = vertices.xy[0][:-1]
+    y_coords = vertices.xy[1][:-1]
+    return x_coords, y_coords
+
+
+def intrude_box(box1, box1_x1, box1_y1, box1_x2, box1_y2, min_side, max_side):
+    border_thickness = min_side/3
+    p_L_shape = .5 #else make U_shape
+
+    box2_x1 = np.random.uniform(border_thickness, box1_x2-border_thickness)
+    box2_y1 = np.random.uniform(border_thickness, box1_y2-border_thickness)
+    print(box1_x2)
+    if np.random.uniform() < p_L_shape:
+        box2_x2 = box2_y2 = max_side
+    else:
+        box2_x2 = np.random.uniform(box2_x1+border_thickness, box1_x2-border_thickness)
+        box2_y2 = max_side
+
+    box2 = geo.box(box2_x1, box2_y1, box2_x2, box2_y2)
+    vertices = box1.difference(box2).exterior.coords
+
+    x_coords = vertices.xy[0][:-1]
+    y_coords = vertices.xy[1][:-1]
+    return x_coords, y_coords
--- code/tests/test_generate_room.py
+++ code/tests/test_generate_room.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 floor = np.array(gr.generate_floor(3, 17))
 
 #floor = np.array([[0, 0, 6, 6, 3, 3], [0, 3, 3, 1.5, 1.5, 0]])
-room = pra.Room.from_corners(floor, fs=16000, max_order=2, absorption=0.1)
+room = pra.Room.from_corners(floor, fs=16000, max_order=2, absorption=0.1
 room.extrude(2.4)
 room.add_source([1.5, 1.2, 1.6])
 R = np.array([[3.,4.2], [2.25, 2.1], [1.4, 1.4]])
