9632b8f36a83feba0695f32e09531e78d417369f
Erik <eriklrssn7@gmail.com>
2018-09-05 13:58:18 +0200

    roomgen can now generate rooms with 1 source and any number of mics, the test file generates a room, performs mirror image model and plots it.

--- code/roomgen.py
+++ code/roomgen.py
@@ -3,44 +3,47 @@ import shapely.geometry as geo
 import pyroomacoustics as pra
 import matplotlib.pyplot as plt
 
-#TODO fix height control of source and receiver
-#TODO clean up code!!!
 
-def generate(min_side, max_side, min_height, max_height):
+def generate(min_side, max_side, min_height, max_height, n_mics, fs=16000,
+        max_order=2, absorption=0.1):
     floor_shape = generate_floor_shape(min_side, max_side)
     height = np.random.uniform(min_height, max_height)
 
-    mic_pos = find_valid_pos(floor_shape)
-    source_pos = find_valid_pos(floor_shape)
-
     vertices = floor_shape.exterior.coords
-
     x_coords = vertices.xy[0][:-1]
     y_coords = vertices.xy[1][:-1]
 
-
-    room = pra.Room.from_corners([x_coords, y_coords], fs=16000, max_order=2, absorption=0.1)
+    room = pra.Room.from_corners([x_coords, y_coords], fs=fs,
+            max_order=max_order, absorption=absorption)
     room.extrude(height)
 
-    print(source_pos[0])
-    room.add_source([source_pos[0],source_pos[1], 1])
+    mic_pos = find_valid_pos(floor_shape, height, n_mics)
+    source_pos = find_valid_pos(floor_shape, height, n_pos=1)
+    source_pos = sum(source_pos, [])
 
-    room.plot()
-    plt.show()
+    mic_array = pra.MicrophoneArray(mic_pos, room.fs)
+    room.add_microphone_array(mic_array)
+    room.add_source(source_pos)
+    return room
 
-def find_valid_pos(floor_shape):
+
+def find_valid_pos(floor_shape, height, n_pos):
+    min_gap = .5
     point_limits = floor_shape.bounds[2:4]
-    print(point_limits)
-    pos_found = False
-    while not pos_found:
-        point_x = np.random.uniform(0, point_limits[0])
-        point_y = np.random.uniform(0, point_limits[1])
-        diluted_point = geo.Point(point_x, point_y).buffer(.1)
-        if floor_shape.contains(diluted_point):
-            pos_found = True
-    return point_x, point_y
-
-
+    x_array = []
+    y_array = []
+    for n in range(n_pos):
+        pos_found = False
+        while not pos_found:
+            point_x = np.random.uniform(0, point_limits[0])
+            point_y = np.random.uniform(0, point_limits[1])
+            diluted_point = geo.Point(point_x, point_y).buffer(min_gap)
+            if floor_shape.contains(diluted_point):
+                pos_found = True
+                x_array.append(point_x)
+                y_array.append(point_y)
+    z_array = np.random.uniform(min_gap, height-min_gap, n_pos).tolist()
+    return x_array, y_array, z_array
 
 
 def generate_floor_shape(min_side, max_side):
@@ -50,9 +53,11 @@ def generate_floor_shape(min_side, max_side):
     box = geo.box(box_x1, box_y1, box_x2, box_y2)
 
     if np.random.uniform() < p_add:
-        floor_shape = add_box(box, box_x1, box_y1, box_x2, box_y2, min_side, max_side)
+        floor_shape = add_box(box, box_x1, box_y1, box_x2, box_y2, min_side,
+                max_side)
     else:
-        floor_shape = subtract_box(box, box_x1, box_y1, box_x2, box_y2, min_side, max_side)
+        floor_shape = subtract_box(box, box_x1, box_y1, box_x2, box_y2,
+                min_side, max_side)
 
     return floor_shape
 
@@ -81,7 +86,8 @@ def subtract_box(box1, box1_x1, box1_y1, box1_x2, box1_y2, min_side, max_side):
     if np.random.uniform() < p_L_shape:
         box2_x2 = box2_y2 = max_side
     else:
-        box2_x2 = np.random.uniform(box2_x1+border_thickness, box1_x2-border_thickness)
+        box2_x2 = np.random.uniform(box2_x1+border_thickness,
+                box1_x2-border_thickness)
         box2_y2 = max_side
 
     box2 = geo.box(box2_x1, box2_y1, box2_x2, box2_y2)
--- code/tests/test_generate_room.py
+++ code/tests/test_generate_room.py
@@ -1,22 +1,13 @@
 import sys
-sys.path.append('../')
-import roomgen as rg
+#sys.path.append('../')
+import rirnet.roomgen as rg
 import pyroomacoustics as pra
 import numpy as np
 import matplotlib.pyplot as plt
 
-#floor = np.array(rg.generate_floor(3, 17))
-room = rg.generate(min_side=3, max_side=17, min_height=2, max_height=3)
-
-#floor = np.array([[0, 0, 6, 6, 3, 3], [0, 3, 3, 1.5, 1.5, 0]])
-#room = pra.Room.from_corners(floor, fs=16000, max_order=2, absorption=0.1)
-#room.extrude(2.4)
-#room.add_source([1.5, 1.2, 1.6])
-#R = np.array([[3.,4.2], [2.25, 2.1], [1.4, 1.4]])
-#bf = pra.MicrophoneArray(R, room.fs)
-#room.add_microphone_array(bf)
-#room.image_source_model()
-#room.plot(img_order=3, aspect='equal')
-#fig = plt.figure()
-#room.plot_rir()
-#plt.show()
+room = rg.generate(min_side=3, max_side=17, min_height=2, max_height=3, n_mics=5)
+room.image_source_model(use_libroom=True)
+room.plot(img_order=3, aspect='equal')
+fig = plt.figure()
+room.plot_rir()
+plt.show()
