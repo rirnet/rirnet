4018173ec4acfdbebe3d87edeb85479a06484f4a
Felix Viberg <felixwiberg@gmail.com>
2018-09-06 16:26:47 +0200

    Added test for handling wavs and mfcc stuff. Added new module acoustic_utils where stuff regarding handling of audio goes.

---
+++ rirnet/acoustic_utils.py
@@ -0,0 +1,53 @@
+import numpy as np
+import librosa
+import scipy as sp
+import os
+
+
+def mfcc_to_waveform(mfcc, rate, waveform_length):
+    n_mel = 128
+    n_fft = 2048
+    n_mfcc = mfcc.shape[0]
+
+    dctm = librosa.filters.dct(n_mfcc, n_mel)
+    mel_basis = librosa.filters.mel(rate, n_fft)
+
+    bin_scaling = 1.0/np.maximum(0.0005, np.sum(np.dot(mel_basis.T, mel_basis),
+    axis=0))
+
+    dctmmfcc = 10.**(np.dot(dctm.T, mfcc)/10.)
+    recon_stft = bin_scaling[:, np.newaxis] * np.dot(mel_basis.T, dctmmfcc)
+
+    excitation = np.random.randn(waveform_length)
+    E = librosa.stft(excitation)
+    recon = librosa.istft(E/np.abs(E)*np.sqrt(recon_stft))
+    return recon
+
+
+def waveform_to_mfcc(waveform, rate, n_mfcc):
+    return librosa.feature.mfcc(waveform, sr=rate, n_mfcc=n_mfcc)
+
+
+def convolve(x, h):
+    return sp.signal.fftconvolve(x,h)
+
+
+def next_power_of_two(x):
+    return 1 if x == 0 else 2**(x-1).bit_length()
+
+
+def normalize(x):
+    return x / np.max(np.abs(x))
+
+
+def play_file(path):
+    os.system('cvlc '+ path +' --play-and-exit')
+
+
+def save_wav(path, data, rate):
+    librosa.output.write_wav(path, data, rate)
+
+
+def read_wav(path):
+    data, rate = librosa.load(path, sr=None)
+    return data, rate
---
+++ rirnet/tests/mfcc.py
@@ -0,0 +1,33 @@
+import rirnet.acoustic_utils as au
+import rirnet.roomgen as rg
+import pyroomacoustics as pra
+import numpy as np
+
+c=340
+max_order=6
+max_side=30
+fs=44100
+
+x, rate = au.read_wav('/home/felix/Downloads/drums.wav')
+
+room = rg.generate(max_order = max_order, min_side=10, max_side=max_side, min_height=2, max_height=3, n_mics=1, fs=44100, absorption = 0.1)
+room.compute_rir()
+
+x = au.normalize(x)
+
+h = room.rir[0][0]
+waveform_length = au.next_power_of_two(np.size(h))
+h = np.pad(h, (0, waveform_length-np.size(h)), 'edge')
+
+mfcc = au.waveform_to_mfcc(h, rate, n_mfcc = 30)
+recon = au.mfcc_to_waveform(mfcc, rate, waveform_length)
+
+output = au.convolve(x,h)
+output = au.normalize(output)
+output_mfcc = au.convolve(x,recon)
+output_mfcc = au.normalize(output_mfcc)
+
+au.save_wav('test_no_mfcc.wav', output, rate)
+au.save_wav('test_mfcc.wav', output_mfcc, rate)
+
+au.play_file('test_mfcc.wav')
