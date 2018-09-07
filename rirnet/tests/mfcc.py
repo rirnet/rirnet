59dbc36a5ed540109e2624017ab53f620bcd9554
Felix <felixwiberg@gmail.com>
2018-09-07 10:56:51 +0200

    initial commit for 6

--- rirnet/tests/mfcc.py
+++ rirnet/tests/mfcc.py
@@ -4,11 +4,11 @@ import pyroomacoustics as pra
 import numpy as np
 
 c=340
-max_order=6
+max_order=11
 max_side=30
 fs=44100
 
-x, rate = au.read_wav('/home/felix/Downloads/drums.wav')
+x, rate = au.read_wav('../../../Downloads/drums.wav')
 
 room = rg.generate(max_order = max_order, min_side=10, max_side=max_side, min_height=2, max_height=3, n_mics=1, fs=44100, absorption = 0.1)
 room.compute_rir()
@@ -31,3 +31,4 @@ au.save_wav('test_no_mfcc.wav', output, rate)
 au.save_wav('test_mfcc.wav', output_mfcc, rate)
 
 au.play_file('test_mfcc.wav')
+au.play_file('test_no_mfcc.wav')
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='rirnet', packages=['rirnet'])
+setup(name='rirnet', packages=['rirnet'], version='0.1')
