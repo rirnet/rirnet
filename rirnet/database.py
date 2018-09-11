c55e673f81eefef008b323fc35cf0a8d1eb291d7
Felix Viberg <felixwiberg@gmail.com>
2018-09-11 12:06:27 +0200

    added exception to database.py

--- rirnet/database.py
+++ rirnet/database.py
@@ -32,7 +32,7 @@ def build_db(root):
             for i_rir, rir in enumerate(room.rir):
                 y = au.convolve(x, rir[0])
                 mfcc_y = au.waveform_to_mfcc(y, rate, n_mfcc)
-                mfcc_h = au.waveform_to_mfcc(rir[0], rate, n_mfcc)                
+                mfcc_h = au.waveform_to_mfcc(rir[0], rate, n_mfcc)
                 name_d = 'room%04d_pos%04d_data.npy' % (i_room, i_rir)
                 name_t = 'room%04d_pos%04d_target.npy' % (i_room, i_rir)
                 path_d = os.path.join(root, name_d)
@@ -40,7 +40,7 @@ def build_db(root):
                 np.save(path_d, mfcc_y)
                 np.save(path_t, mfcc_h)
                 writer.writerow([path_d, path_t, room.corners, room.absorption, room.mic_array.R, room.sources[0].position])
-            
+
 
 def parse_yaml(filename):
     with open(filename, 'r') as stream:
@@ -49,4 +49,8 @@ def parse_yaml(filename):
 
 
 if __name__ == "__main__":
-    build_db(sys.argv[1])
+    try:
+        build_db(sys.argv[1])
+    except:
+        print('No argument given. State path to preferred database folder')
+
