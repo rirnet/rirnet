b79acb1771f121c178c43b01621395bff1f74ccf
Felix <felixwiberg@gmail.com>
2018-09-13 17:15:57 +0200

    Fixed h and y length issues, they are now the same length

--- rirnet/database.py
+++ rirnet/database.py
@@ -17,38 +17,43 @@ def build_db(root):
     root = os.path.abspath(root)
     path_db_setup = os.path.join(root, filename_db_setup)
     db_setup = parse_yaml(path_db_setup)
+    n_rooms = db_setup['n_rooms']
     source_audio = db_setup['source_audio']
     n_mfcc = db_setup['n_mfcc']
-    x, rate = au.read_wav(os.path.join(root, audio_path_rel, source_audio))
-    db_setup['fs'] = rate
-    n_rooms = db_setup['n_rooms']
+    rate = db_setup['fs']
+    x, rate = au.read_wav(os.path.join(root, audio_path_rel, source_audio), rate)
 
+    h_list = []
+    info = []
+    room_pos_number = []
+    for i_room in range(n_rooms):
+        room = rg.generate_from_dict(db_setup)
+        room.compute_rir()
+        for pos, rir in enumerate(room.rir):
+            h_list.append(rir[0])
+            info.append([room.corners, room.absorption, room.mic_array.R, room.sources[0].position])
+            room_pos_number.append([i_room, pos])
 
-    #TODO make sure that conv(x,h) always have the same length and is of power 2
-    #TODO make sure h is of power 2
-    #TODO calculate a reasonable waveform_length from the longest h obtained IN TOTAL!!!
-    #TODO in general; STANDARDIZE THESE SIZES!
+    longest_irf = au.next_power_of_two(len(max(h_list, key=len)))
+    h_list = [au.pad_to(h, longest_irf) for h in h_list]
+    
     with open(os.path.join(root, 'db.csv'), 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(header)
-        for i_room in range(n_rooms):
-            room = rg.generate_from_dict(db_setup)
-            room.compute_rir()
-            for i_rir, rir in enumerate(room.rir):
-                h = rir[0]
-                waveform_length = au.next_power_of_two(rate) #corresponds to 1s h
-                h = np.pad(h, (0, waveform_length-np.size(h)), 'edge')
-                y = au.convolve(x, h)
-                h = np.pad(h, (0, np.size(y) - np.size(h)), 'edge')
-                mfcc_y = au.waveform_to_mfcc(y, rate, n_mfcc)
-                mfcc_h = au.waveform_to_mfcc(h, rate, n_mfcc)
-                name_d = 'room%04d_pos%04d_data.npy' % (i_room, i_rir)
-                name_t = 'room%04d_pos%04d_target.npy' % (i_room, i_rir)
-                path_d = os.path.join(root, name_d)
-                path_t = os.path.join(root, name_t)
-                np.save(path_d, mfcc_y)
-                np.save(path_t, mfcc_h)
-                writer.writerow([path_d, path_t, room.corners, room.absorption, room.mic_array.R, room.sources[0].position])
+        for i_h, h in enumerate(h_list):
+            y = au.convolve(x, h)
+            y_length = au.next_power_of_two(np.size(y))
+            y = au.pad_to(y, y_length)
+            h = au.pad_to(h, y_length)
+            mfcc_y = au.waveform_to_mfcc(y, rate, n_mfcc)
+            mfcc_h = au.waveform_to_mfcc(h, rate, n_mfcc)
+            name_d = 'room%04d_pos%04d_data.npy' % (room_pos_number[i_h][0], room_pos_number[i_h][1])
+            name_t = 'room%04d_pos%04d_target.npy' % (room_pos_number[i_h][0], room_pos_number[i_h][1])
+            path_d = os.path.join(root, name_d)
+            path_t = os.path.join(root, name_t)
+            np.save(path_d, mfcc_y)
+            np.save(path_t, mfcc_h)
+            writer.writerow([path_d, path_t, info[i_h]])
 
 
 def parse_yaml(filename):
