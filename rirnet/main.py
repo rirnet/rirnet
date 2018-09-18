bf62e08e4dac00c031d99f8500e12f62b69647c5
Felix Viberg <felixwiberg@gmail.com>
2018-09-18 09:26:22 +0200

    new version of main

--- rirnet/main.py
+++ rirnet/main.py
@@ -6,11 +6,13 @@ import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import os
-from datetime import datetime
+import csv
+import pandas as pd
+from datetime import datetime, timedelta
+import time
 import numpy as np
 from importlib import import_module
 from glob import glob
-from skimage.color import rgb2luv, luv2rgb
 
 #TODO this code needs love, 'plt_loss_vector' and similar needs better names.
 #TODO constants should be added to self.args in net_master.py
@@ -32,13 +34,13 @@ class Model:
 
         list_epochs = glob('*.pth')
         if list_epochs == []:
-            start_epoch = 1
+            epoch = 0
         else:
-            start_epoch = max([int(e.split('.')[0]) for e in list_epochs])
-            self.model.load_state_dict(torch.load(os.path.join(model_dir, '{}.pth'.format(str(start_epoch)))))
-            start_epoch += 1
+            epoch = max([int(e.split('.')[0]) for e in list_epochs])
+            self.model.load_state_dict(torch.load(os.path.join(model_dir, '{}.pth'.format(str(epoch)))))
 
-        self.epoch = start_epoch
+
+        self.epoch = epoch
         self.csv_path = os.path.join(self.args.db_path, 'db.csv')
         data_transform = self.model.transform()
         train_db = RirnetDatabase(csv_file=self.csv_path, root_dir=self.args.db_path, transform=data_transform)
@@ -49,20 +51,21 @@ class Model:
         self.optimizer = optim.SGD(self.model.parameters(), lr=self.args.lr, momentum=self.args.momentum)
 
 
+
     def train(self):
         self.model.train()
         for batch_idx, (source, target) in enumerate(self.train_loader):
             source, target = source.to(self.device), target.to(self.device)
+            #source = source.cuda(args.gpu, non_blocking=True)
             self.optimizer.zero_grad()
             output = self.model(source)
-            loss = F.mse_loss(output, target)
-            loss.backward()
+            self.loss = F.mse_loss(output, target)
+            self.loss.backward()
             self.optimizer.step()
             if batch_idx % self.args.log_interval == 0:
                 print('Train Epoch: {:5d} [{:5d}/{:5d} ({:4.1f}%)]\tLoss: {:.6f}'.format(
-                    self.epoch, batch_idx * len(source), len(self.train_loader.dataset),
-                    100. * batch_idx / len(self.train_loader), loss.item()))
-
+                    self.epoch + 1, batch_idx * len(source), len(self.train_loader.dataset),
+                    100. * batch_idx / len(self.train_loader), self.loss.item()))
 
     def test(self):
         self.model.eval()
@@ -83,20 +86,79 @@ class Model:
                 #    100. * correct / len(test_loader.dataset)))
 
 
-    def save(self):
+    def save_model(self):
         full_path = os.path.join(self.model_dir, '{}.pth'.format(str(self.epoch)))
         torch.save(self.model.state_dict(), full_path)
 
 
+    def loss_to_file(self):
+        with open('loss_over_epochs.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            if self.args.save_timestamps:
+                writer.writerow([self.epoch, self.loss.item(), datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
+            else:
+                writer.writerow([self.epoch, self.loss.item()])
+
+    def generate_plot(self):
+        frmt = "%Y-%m-%d %H:%M:%S"
+        plot_data = pd.read_csv('loss_over_epochs.csv', header=None)
+        epochs_raw, losses_raw, times_raw = plot_data.values[:,0], plot_data.values[:,1], plot_data.values[:,2]
+
+        epochs = [int(epoch) for epoch in list(plot_data.values[:, 0]) if is_number(epoch)]
+        losses = [float(loss) for loss in list(plot_data.values[:, 1]) if is_number(loss)]
+
+        if self.args.save_timestamps:
+            total_time = timedelta(0, 0, 0)
+            if np.size(times_raw) > 1:
+                start_times = times_raw[epochs_raw == 'started']
+                stop_times = times_raw[epochs_raw == 'stopped']
+                for i_stop_time, stop_time in enumerate(stop_times):
+                    total_time += datetime.strptime(stop_time, frmt) - datetime.strptime(start_times[i_stop_time], frmt)
+                total_time += datetime.now() - datetime.strptime(start_times[-1], frmt)
+                plt.title('Trained for {} hours and {:2d} minutes'.format(int(total_time.days/24 + total_time.seconds//3600), (total_time.seconds//60)%60))
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss (MSE)')
+        plt.semilogy(epochs, losses)
+        plt.savefig('loss_over_epochs.png')
+        plt.close()
+
+
+    def stop_session(self):
+        with open('loss_over_epochs.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['stopped', 'stopped', datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
+
+    def start_session(self):
+        with open('loss_over_epochs.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['started', 'started', datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
+
 def main(model_dir):
     model = Model(model_dir)
+    model.start_session()
+    try:
+        for epoch in range(model.epoch, model.args.epochs + 1):
+            model.train()
+            model.epoch = epoch+1
+            model.loss_to_file()
+            model.generate_plot()
+            if epoch % model.args.save_interval == 0:
+                model.save_model()
+    except KeyboardInterrupt:
+        print(' '+'-'*64, '\nEarly stopping\n', '-'*64)
+        model.stop_session()
+        model.save_model()
+        #model.loss_to_file()
+        #model.generate_plot()
 
-    for epoch in range(model.epoch, model.args.epochs + 1):
-        model.epoch = epoch
-        model.train()
-        if epoch % model.args.save_interval == 0:
-            model.save()
-        # test()
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 
 if __name__ == '__main__':
