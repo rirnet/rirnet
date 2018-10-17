155c7d7e1aae624e944fed830f7afc9d503f8d06
Felix <felixwiberg@gmail.com>
2018-10-17 10:55:08 +0200

    decodes abs_rir every second time

--- peaks_GAN_abstract/rirnet/main.py
+++ peaks_GAN_abstract/rirnet/main.py
@@ -22,128 +22,135 @@ class Model:
     def __init__(self, model_dir):
         self.model_dir = model_dir
         sys.path.append(model_dir)
-        generator = import_module('generator')
-        discriminator = import_module('discriminator')
-        refiner = import_module('refiner')
-        self.G = generator.Net()
-        self.D = discriminator.Net()
+        generator_rir = import_module('generator_rir')
+        generator_sig = import_module('generator_sig')
+        refiner = import_module('decoder')
+        self.GS = generator_sig.Net()
+        self.GR = generator_rir.Net()
         self.R = refiner.Net()
-        self.G_args = self.G.args()
-        self.D_args = self.D.args()
+        self.GS_args = self.GS.args()
+        self.GR_args = self.GR.args()
         self.R_args = self.R.args()
 
-        use_cuda = not self.G_args.no_cuda and torch.cuda.is_available()
+        use_cuda = not self.GS_args.no_cuda and torch.cuda.is_available()
         self.device = torch.device("cuda" if use_cuda else "cpu")
-        self.G.to(self.device)
-        self.D.to(self.device)
+        self.GS.to(self.device)
+        self.GR.to(self.device)
         self.R.to(self.device)
         self.kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
         list_epochs = glob('*.pth')
-        list_epochs = [ x for x in list_epochs if "Go" not in x ]
-        self.G_optimizer = optim.SGD(self.G.parameters(), lr=self.G_args.lr, momentum=self.G_args.momentum, nesterov=True)
-        self.D_optimizer = optim.SGD(self.D.parameters(), lr=self.D_args.lr, momentum=self.D_args.momentum, nesterov=True)
+        list_epochs = [ x for x in list_epochs if "GSo" not in x ]
+        self.GS_optimizer = optim.SGD(self.GS.parameters(), lr=self.GS_args.lr, momentum=self.GS_args.momentum, nesterov=True)
+        self.GR_optimizer = optim.SGD(self.GR.parameters(), lr=self.GR_args.lr, momentum=self.GR_args.momentum, nesterov=True)
         self.R_optimizer = optim.SGD(self.R.parameters(), lr=self.R_args.lr, momentum=self.R_args.momentum, nesterov=True)
         if list_epochs == []:
             epoch = 0
         else:
             epoch = max([int(e.split('_')[0]) for e in list_epochs])
-            self.G_optimizer.load_state_dict(torch.load(os.path.join(model_dir, '{}_Go.pth'.format(str(epoch)))))
-            self.G.load_state_dict(torch.load(os.path.join(model_dir, '{}_G.pth'.format(str(epoch)))))
-            self.D_optimizer.load_state_dict(torch.load(os.path.join(model_dir, '{}_Do.pth'.format(str(epoch)))))
-            self.D.load_state_dict(torch.load(os.path.join(model_dir, '{}_D.pth'.format(str(epoch)))))
+            self.GS_optimizer.load_state_dict(torch.load(os.path.join(model_dir, '{}_GSo.pth'.format(str(epoch)))))
+            self.GS.load_state_dict(torch.load(os.path.join(model_dir, '{}_GS.pth'.format(str(epoch)))))
+            self.GR_optimizer.load_state_dict(torch.load(os.path.join(model_dir, '{}_GRo.pth'.format(str(epoch)))))
+            self.GR.load_state_dict(torch.load(os.path.join(model_dir, '{}_GR.pth'.format(str(epoch)))))
             self.R_optimizer = optim.SGD(self.R.parameters(), lr=self.R_args.lr, momentum=self.R_args.momentum, nesterov=True)
             #self.R_optimizer.load_state_dict(torch.load(os.path.join(model_dir, '{}_Ro.pth'.format(str(epoch)))))
             #self.R.load_state_dict(torch.load(os.path.join(model_dir, '{}_R.pth'.format(str(epoch)))))
-            self.G.cuda()
-            self.D.cuda()
+            self.GS.cuda()
+            self.GR.cuda()
             self.R.cuda()
-            for g in self.G_optimizer.param_groups:
-                g['lr'] = self.G_args.lr
-                g['momentum'] = self.G_args.momentum
-            for g in self.D_optimizer.param_groups:
-                g['lr'] = self.D_args.lr
-                g['momentum'] = self.D_args.momentum
+            for g in self.GS_optimizer.param_groups:
+                g['lr'] = self.GS_args.lr
+                g['momentum'] = self.GS_args.momentum
+            for g in self.GR_optimizer.param_groups:
+                g['lr'] = self.GR_args.lr
+                g['momentum'] = self.GR_args.momentum
             for g in self.R_optimizer.param_groups:
                 g['lr'] = self.R_args.lr
                 g['momentum'] = self.R_args.momentum
 
 
         self.epoch = epoch
-        self.csv_path = os.path.join(self.G_args.db_path, 'db.csv')
-        data_transform = self.G.data_transform()
-        target_transform = self.G.target_transform()
+        self.csv_path = os.path.join(self.GS_args.db_path, 'db.csv')
+        data_transform = self.GS.data_transform()
+        target_transform = self.GS.target_transform()
 
-        train_db = RirnetDatabase(is_training = True, args = self.G_args, data_transform = data_transform, target_transform = target_transform)
-        eval_db = RirnetDatabase(is_training = False, args = self.G_args, data_transform = data_transform, target_transform = target_transform)
-        self.train_loader = torch.utils.data.DataLoader(train_db, batch_size=self.G_args.batch_size, shuffle=True, **self.kwargs)
-        self.eval_loader = torch.utils.data.DataLoader(eval_db, batch_size=self.G_args.batch_size, shuffle=True, **self.kwargs)
+        train_db = RirnetDatabase(is_training = True, args = self.GS_args, data_transform = data_transform, target_transform = target_transform)
+        eval_db = RirnetDatabase(is_training = False, args = self.GS_args, data_transform = data_transform, target_transform = target_transform)
+        self.train_loader = torch.utils.data.DataLoader(train_db, batch_size=self.GS_args.batch_size, shuffle=True, **self.kwargs)
+        self.eval_loader = torch.utils.data.DataLoader(eval_db, batch_size=self.GS_args.batch_size, shuffle=True, **self.kwargs)
 
-        self.G_mean_train_loss = 0
-        self.G_mean_eval_loss = 0
+        self.GS_mean_train_loss = 0
+        self.GS_mean_eval_loss = 0
 
         try:
-            getattr(F, self.G_args.loss_function)
+            getattr(F, self.GS_args.loss_function)
         except AttributeError:
             print('AttributeError! {} is not a valid loss function. The string must exactly match a pytorch loss '
-                  'function'.format(self.G_args.loss_function))
+                  'function'.format(self.GS_args.loss_function))
             sys.exit()
 
 
     def train(self):
-        self.G.train()
-        self.D.train()
+        self.GS.train()
+        self.GR.train()
         self.R.train()
         l1_loss_list = []
         l2_loss_list = []
+        l3_loss_list = []
         for batch_idx, (source, target) in enumerate(self.train_loader):
             torch.cuda.empty_cache()
 
             source, target = source.to(self.device), target.to(self.device)
 
-            self.G_optimizer.zero_grad()
-            self.D_optimizer.zero_grad()
-            abs_gen_rir = self.G(source)
-            abs_real_rir = self.D(target).detach()
-            l1 = self.hausdorff(abs_gen_rir, abs_real_rir)
-            l1.backward(retain_graph = True)
-            self.G_optimizer.step()
-
-            abs_real_rir = self.D(target)
-            abs_gen_rir = self.G(source).detach()
-            l11 = self.hausdorff(abs_real_rir, abs_gen_rir)
-            l11.backward(retain_graph = True)
-            self.D_optimizer.step()
-
-
-            abs_gen_rir = self.G(source)
-            abs_real_rir = self.D(target)
-            l1 = self.hausdorff(abs_gen_rir, abs_real_rir)
-            l11 = self.hausdorff(abs_real_rir, abs_gen_rir)
+            self.GS_optimizer.zero_grad()
+            self.GR_optimizer.zero_grad()
             self.R_optimizer.zero_grad()
-            gen_rir = self.R(abs_gen_rir)
-            gen_rir[:,0] = (gen_rir[:,0].clone().t() - gen_rir[:,0,0].clone()).t()
-            l2 = self.hausdorff(gen_rir, target)
 
-            #l2 = getattr(F, self.G_args.loss_function)(gen_rir, target)
+            ##Forward and update GS
+            abs_gen_rir = self.GS(source)
+            abs_real_rir = self.GR(target).detach()
+            l1 = self.hausdorff(abs_gen_rir, abs_real_rir)
             l1.backward(retain_graph = True)
-            l11.backward(retain_graph = True)
-            l2.backward()
+            self.GS_optimizer.step()
 
-            self.G_optimizer.step()
-            self.D_optimizer.step()
+            ##Forward and update GR
+            abs_real_rir = self.GR(target)
+            abs_gen_rir = self.GS(source).detach()
+            l2 = self.hausdorff(abs_real_rir, abs_gen_rir)
+            l2.backward(retain_graph = True)
+            self.GR_optimizer.step()
+
+            ##Forward through GS, GR and R, update all
+            abs_gen_rir = self.GS(source)
+            abs_real_rir = self.GR(target)
+            l1 = self.hausdorff(abs_gen_rir, abs_real_rir)
+            l2 = self.hausdorff(abs_real_rir, abs_gen_rir)
+            if batch_idx%2 == 0:
+                gen_rir = self.R(abs_gen_rir)
+            else:
+                gen_rir = self.R(abs_real_rir)
+            gen_rir[:,0] = (gen_rir[:,0].clone().t() - gen_rir[:,0,0].clone()).t()
+            l3 = self.hausdorff(gen_rir, target)
+            l1.backward(retain_graph = True)
+            l2.backward(retain_graph = True)
+            l3.backward()
+
+            self.GS_optimizer.step()
+            self.GR_optimizer.step()
             self.R_optimizer.step()
 
             l1_loss_list.append(l1.item())
             l2_loss_list.append(l2.item())
+            l3_loss_list.append(l3.item())
 
-            if batch_idx % self.G_args.log_interval == 0:
+            if batch_idx % self.GS_args.log_interval == 0:
                 print('Train Epoch: {:5d} [{:5d}/{:5d} ({:4.1f}%)]\tLoss: {:.6f}, {:.6f}, {:.6f}'.format(
                     self.epoch + 1, batch_idx * len(source), len(self.train_loader.dataset),
-                    100. * batch_idx / len(self.train_loader), l1.item(), l11.item(), l2.item()))
+                    100. * batch_idx / len(self.train_loader), l1.item(), l2.item(), l3.item()))
 
         self.l1_mean_train_loss = np.mean(l1_loss_list)
         self.l2_mean_train_loss = np.mean(l2_loss_list)
+        self.l3_mean_train_loss = np.mean(l3_loss_list)
 
         target_im = target.cpu().detach().numpy()
         output_im = gen_rir.cpu().detach().numpy()
@@ -151,7 +158,7 @@ class Model:
         abs_gen_rir_im = abs_gen_rir.cpu().detach().numpy()
         abs_real_rir_im = abs_real_rir.cpu().detach().numpy()
 
-        plt.plot(output_im[0, 0, :], output_im[0, 1, :], '--x', linewidth=0.5, markersize=0.7, label='output')
+        plt.plot(output_im[0, 0, :], output_im[0, 1, :], '--x', linewidth=0.5, markersize=0.7, label='gen_rir')
         plt.plot(target_im[0, 0, :], target_im[0, 1, :], '--o', linewidth=0.5, markersize=0.7, label='target')
         plt.title('train')
         plt.grid(True)
@@ -173,7 +180,7 @@ class Model:
         with torch.no_grad():
             for batch_idx, (source, target) in enumerate(self.eval_loader):
                 source, target = source.to(self.device), target.to(self.device)
-                output = self.R(self.G(source))
+                output = self.R(self.GS(source))
                 output[:,0] = (output[:,0].clone().t() - output[:,0,0].clone()).t()
                 eval_loss = self.hausdorff(output, target).item()
                 eval_loss_list.append(eval_loss)
@@ -193,15 +200,15 @@ class Model:
 
     def save_model(self):
         print(' '+'-'*64, '\nSaving\n', '-'*64)
-        model_full_path = os.path.join(self.model_dir, '{}_G.pth'.format(str(self.epoch)))
-        optimizer_full_path = os.path.join(self.model_dir, '{}_Go.pth'.format(str(self.epoch)))
-        torch.save(self.G.state_dict(), model_full_path)
-        torch.save(self.G_optimizer.state_dict(), optimizer_full_path)
+        model_full_path = os.path.join(self.model_dir, '{}_GS.pth'.format(str(self.epoch)))
+        optimizer_full_path = os.path.join(self.model_dir, '{}_GSo.pth'.format(str(self.epoch)))
+        torch.save(self.GS.state_dict(), model_full_path)
+        torch.save(self.GS_optimizer.state_dict(), optimizer_full_path)
 
-        model_full_path = os.path.join(self.model_dir, '{}_D.pth'.format(str(self.epoch)))
-        optimizer_full_path = os.path.join(self.model_dir, '{}_Do.pth'.format(str(self.epoch)))
-        torch.save(self.D.state_dict(), model_full_path)
-        torch.save(self.D_optimizer.state_dict(), optimizer_full_path)
+        model_full_path = os.path.join(self.model_dir, '{}_GR.pth'.format(str(self.epoch)))
+        optimizer_full_path = os.path.join(self.model_dir, '{}_GRo.pth'.format(str(self.epoch)))
+        torch.save(self.GR.state_dict(), model_full_path)
+        torch.save(self.GR_optimizer.state_dict(), optimizer_full_path)
 
 
     def loss_to_file(self):
@@ -219,7 +226,7 @@ class Model:
         train_losses = [float(loss) for loss in list(train_losses_raw) if is_number(loss)]
         eval_losses = [float(loss) for loss in list(eval_losses_raw) if is_number(loss)]
 
-        if self.G_args.save_timestamps:
+        if self.GS_args.save_timestamps:
             total_time = timedelta(0, 0, 0)
             if np.size(times_raw) > 1:
                 start_times = times_raw[epochs_raw == 'started']
@@ -229,7 +236,7 @@ class Model:
                 total_time += datetime.now() - datetime.strptime(start_times[-1], frmt)
                 plt.title('Trained for {} hours and {:2d} minutes'.format(int(total_time.days/24 + total_time.seconds//3600), (total_time.seconds//60)%60))
         plt.xlabel('Epochs')
-        plt.ylabel('Loss ({})'.format(self.G_args.loss_function))
+        plt.ylabel('Loss ({})'.format(self.GS_args.loss_function))
         plt.semilogy(epochs, train_losses, label='Training Loss')
         plt.semilogy(epochs, eval_losses, label='Evaluation Loss')
         plt.legend()
@@ -274,7 +281,7 @@ def main(model_dir):
     model = Model(model_dir)
     model.start_session()
 
-    for epoch in range(model.epoch, model.G_args.epochs + 1):
+    for epoch in range(model.epoch, model.GS_args.epochs + 1):
         model.train()
         #model.evaluate()
         model.epoch = epoch+1
@@ -286,7 +293,7 @@ def main(model_dir):
         if interrupted:
             print(' '+'-'*64, '\nEarly stopping\n', '-'*64)
             model.stop_session()
-            model.save_model()
+            #model.save_model()
             break
 
 
