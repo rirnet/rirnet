467fedca4a9bba36912a418c8d6c07c4e19b17c9
Felix Viberg <felixwiberg@gmail.com>
2019-02-01 17:39:30 +0100

    added bash scripts to run all trainings in nanonet and collect results

---
+++ nanonet/models/run_all.sh
@@ -0,0 +1,15 @@
+cd 04;
+. run_ae.sh;
+. run_ex.sh;
+cd ../08;
+. run_ae.sh;
+. run_ex.sh;
+cd ../16;
+. run_ae.sh;
+. run_ex.sh;
+cd ../32;
+. run_ae.sh;
+. run_ex.sh;
+cd ../64;
+. run_ae.sh;
+. run_ex.sh;
